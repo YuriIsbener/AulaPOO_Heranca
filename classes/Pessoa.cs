86f9f2f15514ba33aafe5cf44944f0b57d34996e
Yuri Isbener <yurisuhett@gmail.com>
2020-12-07 20:32:52 -0300

    Trab feito

---
+++ classes/Pessoa.cs
@@ -0,0 +1,11 @@
+namespace AulaPOO_Heranca.classes
+{
+    public class Pessoa
+    {
+        public string nome;
+
+        public string DarBoasVindas(string nomeUsuario){
+            return "Seja bem vindo ao nosso sistema, "+nomeUsuario;
+        }
+    }
+}
---
+++ classes/PessoaJuridica.cs
@@ -0,0 +1,19 @@
+namespace AulaPOO_Heranca.classes
+{
+    public class PessoaJuridica : Pessoa
+    {
+        public string cnpj;
+        public string inscricaoEstadual;
+
+        public bool ValidarCNPJ(string documento){
+            
+            if (documento != "")
+            {
+                return true;   
+            }
+
+            return false;
+        }
+
+    }
+}
